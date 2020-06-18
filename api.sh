5ee1567d661ac2f9b7828b71376c7b200482e31a
Ubuntu <ubuntu@ip-172-31-37-112.ap-southeast-1.compute.internal>
2020-06-18 11:30:55 +0000

    latest migration scripts

---
+++ api.sh
@@ -0,0 +1,1 @@
+docker run -it --rm --name api --network flask-api-docker_default -v $(pwd):/app sp33c/python:3.8
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
 pip3 install --upgrade pip 
 #python3 -m pip install flask
 python3 -m pip install -r requirements.txt
+#export FLASK_APP=app:app/app
+cd app
+flask db init
+flask db migrate
+flask db upgrade
 
