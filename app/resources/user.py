12556f1f8fc4a892a14b629a40967d4f16f1455a
Alexander Fitterling <info@sp33c.de>
2020-06-20 22:03:05 +0800

    user resource

--- app/resources/user.py
+++ app/resources/user.py
@@ -7,5 +7,18 @@ from models.user import User
 #from models.user import user
 
 class UserListResource(Resource):
+        def post(self):
+            json_data = request.get_json()
+
+            username = json_data.get('username')
+            email = json_data.get('email')
+            no_hash_password = json_data.get('password')
+
+            if User.get_by_username(username):
+                return {'message': 'username used already'}, HTTPStatus.BAD_REQUEST
+
+            if User.get_by_email(email):
+                return {'message': 'email used already'}, HTTPStatus.BAD_REQUEST
+
         def get(self):
             return jsonify({'data': []})
