5e06f91ba62d330c03aee7824c97925bc5ee6781
Alexander Fitterling <info@sp33c.de>
2020-06-27 19:21:50 +0800

    python container adjustments

--- api.sh
+++ api.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --name api -p5000:5000 --network flask_restful0_default -v $(pwd):/app $* sp33c/python:3.8
+docker run -it --rm --name api -p5000:5000 --network flask_restful0_default -v $(pwd):/work $* sp33c/python:3.8
