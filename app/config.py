f9abb451714653586fab4355ac466c9617520808
Alexander Fitterling <info@sp33c.de>
2020-07-04 14:56:14 +0800

    secret key from env cleanedup

--- app/config.py
+++ app/config.py
@@ -11,14 +11,9 @@ class Config:
                 'pool_pre_ping': True
         }
 
-        # https://www.base64encode.org/
-        
-        SECRET_KEY = 'VmllbGUgZWluc2lsYmlnZSBlbmdsaXNjaGUgTm9tZW4gc2luZCBOb21pbmFsaXNpZXJ1bmdlbiB2b24gVmVyYmVuOyBzaWUgd2VyZGVuIHdpZSBlbnRzcHJlY2hlbmRlIGRldXRzY2hlIEJpbGR1bmdlbiAoZGVyIExhdWYvU3BydW5nOyAKCkhvYmVyZywgUnVkb2xmLEhvYmVyZywgVXJzdWxhLiBEZXV0c2NoZSBHcmFtbWF0aWsgKFNBICJ0byBnbyIpOiBFaW5lIFNwcmFjaGxlaHJlIGbDvHIgQmVydWYsIFN0dWRpdW0sIEZvcnRiaWxkdW5nIHVuZCBBbGx0YWc6IEVpbmUgU3ByYWNobGVocmUgZsO8ciBCZXJ1ZiwgU3R1ZGl1bSwgRm9ydGJpbGR1bmcgdW5kIEFsbHRhZyAoRGVyIGtsZWluZSBEdWRlbikgKEdlcm1hbiBFZGl0aW9uKSAoS2luZGxlLVBvc2l0aW9uZW40NTczLTQ1NzQpLiBEdWRlbi4gS2luZGxlLVZlcnNpb24uIA=='\
-                if not os.environ.get('SECRET_KEY') else os.environ.get('SECRET_KEY')
-        #SECRET_KEY = os.environ.get('SECRET_KEY')
+        SECRET_KEY = os.environ.get('SECRET_KEY')
         
         JWT_ERROR_MESSAGE_KEY = 'message'
-
         JWT_BLACKLIST_ENABLED = True
         JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
         
